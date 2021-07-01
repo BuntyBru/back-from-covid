38fce1c79cf87332db6c4ee2aa9229cb9224f03a
BuntyBru <buntybru@gmail.com>
2021-07-01 16:10:24 +0530

    Error Boundary and StufF

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import { render } from "react-dom";
-import { StrictMode } from "react";
-
+import { useState,StrictMode } from "react";
+import Details from "./Details";
+import { BrowserRouter as Router,Route,Switch,Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
+    <ThemeContext.Provider value={theme}>
     <div>
-      <h1>Adopt Me!</h1>
+        <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+
+        <Switch>
+
+        <Route path = "/details/:id">
+        <Details/>
+      </Route>
+      <Route path = "/">
       <SearchParams />
+      </Route> 
+
+
+        </Switch>
+    
+      </Router>
+       
+ 
     </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useContext } from "react";
 //import Pet from "./Pet";
 import useBreedList from "./useBreedList";
 const ANIMALS = ['bird','cat','dog','rabbit','reptile'];
 import Results from "./Results";
+import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
   const [location, setLocation] = useState('');
@@ -11,6 +12,7 @@ const SearchParams = () => {
   const [pets,setPets ] =useState([]);
 
   const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
 
 
   useEffect(()=>{
@@ -72,7 +74,21 @@ const SearchParams = () => {
 
         </label>
 
-        <button>Submit</button>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
 
       <Results pets={pets}/>
